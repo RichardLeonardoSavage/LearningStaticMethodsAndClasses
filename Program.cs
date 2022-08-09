c680b85cd7c74c537806684242a3c3177553f126
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-09 18:12:48 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningStaticMethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsefulTools.SayHi("Mike");
+
+
+            //note that these 2 static class methods don't need to/CAN'T have instances created but their methods can still be invoked
+            Console.WriteLine(Math.Sqrt(144));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UsefulTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningStaticMethodsAndClasses
+{
+
+
+    //when you create a static class you cannon create instances of it but you can still invoke methods from the class. A good example of this is the math class
+     static class UsefulTools
+    {
+        public static void SayHi(string name)
+        { Console.WriteLine("Hello " + name); }
+    }
+}
